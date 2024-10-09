7bed9922f68e65d4ccc7268da2f1a42410cae25e
unknown <chaerulanwar428@gmail.com>
2024-10-09 22:47:44 +0700

    tambah component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './component/Header';
+import Hero from './component/Hero';
+import Browse from './component/Browse';
+import Arrived from './component/Arrived';
+import Clients from './component/Clients';
+import Footer from './component/Footer';
+
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Browse/>
+    <Arrived/>
+    <Clients/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer class="flex text-center px-4 py-8 justify-center">
+        <p class="text-sm">
+          Copyright 2024 • All Rights Reserved E-commerce LuxSpace by Chaerulanwar
+        </p>
+      </footer>
+    )
+}
+
+export default Footer;
